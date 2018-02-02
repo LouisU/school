f27db846ca750546ca0cf97717e25373599c71db
louis <1056699051@qq.com>
2018-02-02 17:33:44 +0800

    add model user, going to do test about grab data from sqlserver in model user

--- honghua/views.py
+++ honghua/views.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+import pymssql
 from django.shortcuts import render
 from . import models
 # Create your views here.
@@ -8,13 +9,14 @@ from . import models
 
 def testmssql(request):
 
-    user = models.User.objects.filter(name='a')
-    print user[0].cardno
-    return True
+    # user = models.User.objects.filter(name='a')
+    # print user[0].cardno
+    # return True
 
-#     conn = pymssql.connect('10.66.223.205', 'mhmt', 'Meihao365.net', 'Qizhong')
-#     cursor = conn.cursor()
-#     cursor = cursor.execute("select username from Sys_UserInfo where sys_userinfoid=7")
-#     list = cursor.fetchone()
-#     cursor.close()
-#     return list
+    conn = pymssql.connect(u'111.231.235.153:1234', u'mhmt', u'Meihao365.net', u'Qizhong')
+    cursor = conn.cursor()
+    cursor.execute("select * from Sys_UserInfo where sys_userinfoid=7")
+    list = cursor.fetchone()
+    print list
+    cursor.close()
+    return list
